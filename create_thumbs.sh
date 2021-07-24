309c95e8b4776470c6b8ee6170f093b687774d8e
Joren Van Onder <joren@jvo.sh>
2021-07-24 11:33:01 -0700

    Increase thumbnail quality

--- create_thumbs.sh
+++ create_thumbs.sh
@@ -7,6 +7,6 @@ for IMG in "${DIR}"/*; do
   if [[ ! $IMG =~ "_thumb" ]]; then
     echo "Creating thumbnail for ${IMG}..."
     WITHOUT_EXT="${IMG%%.*}"
-    convert -thumbnail 500x500 "${IMG}" "${WITHOUT_EXT}_thumb.webp"
+    convert -thumbnail 1500x -quality 75 "${IMG}" "${WITHOUT_EXT}_thumb.webp"
   fi
 done
