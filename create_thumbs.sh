5ff623a94f64abcdf0eacf24975bb913e862960c
Joren Van Onder <joren@jvo.sh>
2021-07-26 20:49:12 -0700

    Ignore videos for thumbnails

--- create_thumbs.sh
+++ create_thumbs.sh
@@ -4,9 +4,12 @@ set -euo pipefail
 DIR="${1}"
 
 for IMG in "${DIR}"/*; do
-  if [[ ! $IMG =~ "_thumb" ]]; then
-    echo "Creating thumbnail for ${IMG}..."
+    if [[ ! $IMG =~ "_thumb" ]] && [[ ! $IMG =~ ".mp4" ]]; then
     WITHOUT_EXT="${IMG%%.*}"
-    convert -thumbnail 1500x -quality 75 "${IMG}" "${WITHOUT_EXT}_thumb.webp"
+    THUMB_NAME="${WITHOUT_EXT}_thumb.webp"
+    if [ ! -f "${THUMB_NAME}" ]; then
+      echo "Creating thumbnail for ${IMG}..."
+      convert -thumbnail 1500x -quality 75 "${IMG}" "${THUMB_NAME}"
+    fi
   fi
 done
