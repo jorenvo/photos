f39d6c242222c4331c9681df9192012c65dc93c6
Joren Van Onder <joren@jvo.sh>
2022-01-12 20:09:29 -0800

    Loading view

--- view.js
+++ view.js
@@ -5,37 +5,58 @@ function setText(id, text) {
   tag.innerText = text;
 }
 
-const url = new URL(window.location.href);
-const image_location = url.searchParams.get("url");
+function loadEXIF(image) {
+  return new Promise(function (resolve, reject) {
+    EXIF.getData(image, function () {
+      console.log(EXIF.pretty(this));
 
-const image = document.getElementById("photo");
-image.src = image_location;
-image.onload = () => {
-  EXIF.getData(image, function () {
-    console.log(EXIF.pretty(this));
+      const date = EXIF.getTag(this, "DateTimeOriginal");
+      const model = EXIF.getTag(this, "Model");
+      const lens = EXIF.getTag(this, "LensModel");
+      const f_number = EXIF.getTag(this, "FNumber");
+      const exposure_time = EXIF.getTag(this, "ExposureTime");
+      const iso = EXIF.getTag(this, "ISOSpeedRatings");
 
-    const date = EXIF.getTag(this, "DateTimeOriginal");
-    const model = EXIF.getTag(this, "Model");
-    const lens = EXIF.getTag(this, "LensModel");
-    const f_number = EXIF.getTag(this, "FNumber");
-    const exposure_time = EXIF.getTag(this, "ExposureTime");
-    const iso = EXIF.getTag(this, "ISOSpeedRatings");
+      setText("exif-camera", model);
+      setText("exif-lens", lens);
+      setText("exif-aperture", `ƒ/${f_number}`);
 
-    setText("exif-camera", model);
-    setText("exif-lens", lens);
-    setText("exif-aperture", `ƒ/${f_number}`);
+      let exposure_string = "";
+      if (exposure_time.denominator === 1) {
+        exposure_string = `${exposure_time.numerator}`;
+      } else {
+        exposure_string = `${exposure_time.numerator}/${exposure_time.denominator}`;
+      }
 
-    let exposure_string = "";
-    if (exposure_time.denominator === 1) {
-      exposure_string = `${exposure_time.numerator}`;
-    } else {
-      exposure_string = `${exposure_time.numerator}/${exposure_time.denominator}`;
-    }
+      setText("exif-exposure", `${exposure_string}s`);
+      setText("exif-iso", iso);
 
-    setText("exif-exposure", `${exposure_string}s`);
-    setText("exif-iso", iso);
-
-    let [year, month, day] = date.split(" ")[0].split(":");
-    setText("exif-date", `${year}/${month}/${day}`);
+      const [year, month, day] = date.split(" ")[0].split(":");
+      setText("exif-date", `${year}/${month}/${day}`);
+      resolve();
+    });
   });
-};
+}
+
+function show() {
+  function toggle(e) {
+    e.classList.toggle("hide");
+  }
+  toggle(document.getElementById("loading"));
+  toggle(document.getElementById("photo"));
+  document.querySelectorAll(".exif-tag").forEach(toggle);
+}
+
+async function view() {
+  const url = new URL(window.location.href);
+  const image_location = url.searchParams.get("url");
+
+  const image = document.getElementById("photo");
+  image.src = image_location;
+  image.onload = async () => {
+    await loadEXIF(image);
+    show();
+  };
+}
+
+view();
