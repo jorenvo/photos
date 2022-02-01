d61b147c7fc54c697fb92a77941f0e95c24caf50
Joren Van Onder <joren@jvo.sh>
2022-01-31 17:36:37 -0800

    Extract Viewer class

--- view.js
+++ view.js
@@ -2,25 +2,17 @@
 
 import { relyOnPinchToZoom, getViewUrl, getPhotoNames } from "./utils.js";
 
-const global_zoom_factor = 180 / 90; // TODO calculate from CSS?
-
-var global_photo = document.getElementById("photo");
-var global_photo_high = document.getElementById("photo-high");
-
-var image_low_url = "";
-var image_high_url = "";
-
 function setText(id, text) {
   const tag = document.getElementById(id);
   tag.innerText = text;
 }
 
-function loadEXIF() {
+function loadEXIF(img) {
   return new Promise(function (resolve, reject) {
     // TODO: better to use the small thumbnail here because
     // the library will re-download the whole image into an
     // arraybuffer.
-    EXIF.getData(global_photo, function () {
+    EXIF.getData(img, function () {
       console.log(EXIF.pretty(this));
 
       const date = EXIF.getTag(this, "DateTimeOriginal");
@@ -51,202 +43,244 @@ function loadEXIF() {
   });
 }
 
-function wireZoom() {
-  if (!relyOnPinchToZoom()) {
-    global_photo.addEventListener("click", zoom);
+class Viewer {
+  constructor(image_low_url, image_high_url) {
+    this.image_low_url = image_low_url;
+    this.image_high_url = image_high_url;
+
+    this.global_photo = document.getElementById("photo");
+    this.global_photo_high = document.getElementById("photo-high");
+
+    this.zoom_factor = 180 / 90; // TODO: calculate from CSS?
+    this.suppressing_mouse_move = false;
+
+    this._bindFunctions();
   }
-}
 
-function unWireZoom() {
-  global_photo.removeEventListener("click", zoom);
-}
-
-function calculateTranslation(image_size, viewport_size, pointer_pos) {
-  const padding = 300;
-  image_size += padding;
-
-  const max_translation = Math.max(0, image_size - viewport_size);
-  const ratio = pointer_pos / viewport_size - 0.5;
-  const translation_mouse = max_translation * ratio;
-  const translation_center = image_size / 2 - viewport_size / 2;
-
-  // console.log("image_size", image_size);
-  // console.log("viewport_size", viewport_size);
-  // console.log("pointer_pos", pointer_pos);
-  // console.log("max_translation", max_translation);
-  // console.log("ratio", ratio);
-  // console.log("translation_mouse", translation_mouse);
-  // console.log("translation_center", translation_center);
-  // console.log("--------");
-
-  return -translation_center - translation_mouse + padding / 2;
-}
-
-var suppressing_mouse_move = false;
-function onZoomMouseMove(e) {
-  const photo = e.target;
-  if (suppressing_mouse_move) {
-    return;
+  _bindFunctions() {
+    this._zoom = this._zoom.bind(this);
+    this._onZoomMouseMove = this._onZoomMouseMove.bind(this);
+    this._stopSmoothZoom = this._stopSmoothZoom.bind(this);
+    this._wireMouseMove = this._wireMouseMove.bind(this);
+    this._swapToHigh = this._swapToHigh.bind(this);
+    this._onResize = this._onResize.bind(this);
+    this._wireZoom = this._wireZoom.bind(this);
+    this._onPhotoLoad = this._onPhotoLoad.bind(this);
   }
-  suppressing_mouse_move = true;
-  setTimeout(() => (suppressing_mouse_move = false), 1_000 / 60); // todo requestAnimationFrame
 
-  const image_rect = photo.getBoundingClientRect();
-  const x_translation = calculateTranslation(
-    image_rect.width,
-    document.body.clientWidth,
-    e.clientX
-  );
-  const y_translation = calculateTranslation(
-    image_rect.height,
-    document.body.clientHeight,
-    e.clientY
-  );
-  photo.style.transform = `translate(${x_translation}px, ${y_translation}px)`;
-}
+  _wireZoom() {
+    if (!relyOnPinchToZoom()) {
+      this.global_photo.addEventListener("click", this._zoom);
+    }
+  }
 
-function stopSmoothZoom(e) {
-  global_photo.classList.remove("smooth-zoom");
-  e.target.removeEventListener("transitionend", stopSmoothZoom);
-}
+  _unWireZoom() {
+    this.global_photo.removeEventListener("click", this._zoom);
+  }
 
-function unWireMouseMove() {
-  global_photo.removeEventListener("mousemove", onZoomMouseMove);
-  global_photo_high.removeEventListener("mousemove", onZoomMouseMove);
-}
+  _calculateTranslation(image_size, viewport_size, pointer_pos) {
+    const padding = 300;
+    image_size += padding;
 
-function wireMouseMove(e) {
-  global_photo.addEventListener("mousemove", onZoomMouseMove);
-  global_photo_high.addEventListener("mousemove", onZoomMouseMove);
-  e.target.removeEventListener("transitionend", wireMouseMove);
-}
+    const max_translation = Math.max(0, image_size - viewport_size);
+    const ratio = pointer_pos / viewport_size - 0.5;
+    const translation_mouse = max_translation * ratio;
+    const translation_center = image_size / 2 - viewport_size / 2;
 
-function initialZoomOnPointer(e) {
-  const image_rect = global_photo.getBoundingClientRect();
-  const x_translation = calculateTranslation(
-    image_rect.width * global_zoom_factor,
-    document.body.clientWidth,
-    e.clientX
-  );
-  const y_translation = calculateTranslation(
-    image_rect.height * global_zoom_factor,
-    document.body.clientHeight,
-    e.clientY
-  );
-  global_photo.style.transform = `translate(${x_translation}px, ${y_translation}px)`;
-}
+    return -translation_center - translation_mouse + padding / 2;
+  }
 
-function wireSwapToLow() {
-  global_photo_high.addEventListener("click", (e) => {
-    global_photo.style.transform = global_photo_high.style.transform;
-    global_photo_high.classList.add("hide");
-    global_photo.classList.remove("hide");
-    setTimeout(() => global_photo.click(), 0);
-  });
-}
+  _onZoomMouseMove(e) {
+    const photo = e.target;
+    if (this.suppressing_mouse_move) {
+      return;
+    }
+    this.suppressing_mouse_move = true;
+    setTimeout(() => (this.suppressing_mouse_move = false), 1_000 / 60); // TODO: requestAnimationFrame
 
-function swapToHigh() {
-  setTimeout(() => {
-    global_photo_high.src = image_high_url;
-    global_photo_high.decode().then(() => {
-      global_photo.classList.add("hide");
+    const image_rect = photo.getBoundingClientRect();
+    const x_translation = this._calculateTranslation(
+      image_rect.width,
+      document.body.clientWidth,
+      e.clientX
+    );
+    const y_translation = this._calculateTranslation(
+      image_rect.height,
+      document.body.clientHeight,
+      e.clientY
+    );
+    photo.style.transform = `translate(${x_translation}px, ${y_translation}px)`;
+  }
 
-      if (relyOnPinchToZoom()) {
-        global_photo_high.classList.replace("photo-zoom", "photo-normal");
+  _stopSmoothZoom(e) {
+    this.global_photo.classList.remove("smooth-zoom");
+    e.target.removeEventListener("transitionend", this._stopSmoothZoom);
+  }
+
+  _unWireMouseMove() {
+    this.global_photo.removeEventListener("mousemove", this._onZoomMouseMove);
+    this.global_photo_high.removeEventListener(
+      "mousemove",
+      this._onZoomMouseMove
+    );
+  }
+
+  _wireMouseMove(e) {
+    this.global_photo.addEventListener("mousemove", this._onZoomMouseMove);
+    this.global_photo_high.addEventListener("mousemove", this._onZoomMouseMove);
+    e.target.removeEventListener("transitionend", this._wireMouseMove);
+  }
+
+  _initialZoomOnPointer(e) {
+    const image_rect = this.global_photo.getBoundingClientRect();
+    const x_translation = this._calculateTranslation(
+      image_rect.width * this.zoom_factor,
+      document.body.clientWidth,
+      e.clientX
+    );
+    const y_translation = this._calculateTranslation(
+      image_rect.height * this.zoom_factor,
+      document.body.clientHeight,
+      e.clientY
+    );
+    this.global_photo.style.transform = `translate(${x_translation}px, ${y_translation}px)`;
+  }
+
+  _wireSwapToLow() {
+    this.global_photo_high.addEventListener("click", (e) => {
+      this.global_photo.style.transform =
+        this.global_photo_high.style.transform;
+      this.global_photo_high.classList.add("hide");
+      this.global_photo.classList.remove("hide");
+      setTimeout(() => this.global_photo.click(), 0);
+    });
+  }
+
+  _swapToHigh() {
+    setTimeout(() => {
+      this.global_photo_high.src = this.image_high_url;
+      this.global_photo_high.decode().then(() => {
+        this.global_photo.classList.add("hide");
+
+        if (relyOnPinchToZoom()) {
+          this.global_photo_high.classList.replace(
+            "photo-zoom",
+            "photo-normal"
+          );
+        }
+
+        this.global_photo_high.style.transform =
+          this.global_photo.style.transform;
+        this.global_photo_high.classList.remove("hide");
+      });
+
+      this.global_photo.removeEventListener("transitionend", this._swapToHigh);
+    }, 50);
+  }
+
+  _zoom(e) {
+    // Cancel high res if zoom level is about to change
+    this.global_photo_high.onload = undefined;
+
+    if (this.global_photo.classList.contains("photo-zoom")) {
+      this._unWireMouseMove();
+
+      this.global_photo.classList.add("smooth-zoom");
+      this.global_photo.classList.replace("photo-zoom", "photo-normal");
+
+      this._centerPhoto(
+        this.global_photo.getBoundingClientRect().width / this.zoom_factor
+      );
+
+      this.global_photo.addEventListener("transitionend", this._stopSmoothZoom);
+    } else {
+      this._unWireZoom();
+      this.global_photo.classList.add("smooth-zoom");
+      this._initialZoomOnPointer(e);
+      this.global_photo.classList.replace("photo-normal", "photo-zoom");
+      this.global_photo.addEventListener("transitionend", this._wireMouseMove);
+      this.global_photo.addEventListener("transitionend", this._stopSmoothZoom);
+      this.global_photo.addEventListener("transitionend", this._swapToHigh);
+      this.global_photo.addEventListener("transitionend", this._wireZoom);
+    }
+  }
+
+  _onResize() {
+    if (this.global_photo.classList.contains("photo-normal")) {
+      this._centerPhoto(this.global_photo.width);
+    }
+  }
+
+  _centerPhoto(width) {
+    this.global_photo.style.transform = `translate(${
+      (document.body.clientWidth - width) / 2
+    }px, 0px)`;
+  }
+
+  _show() {
+    function toggle(e) {
+      e.classList.toggle("hide");
+    }
+
+    toggle(document.getElementById("loading"));
+
+    toggle(this.global_photo);
+    this._centerPhoto(this.global_photo.getBoundingClientRect().width);
+
+    document
+      .querySelectorAll(".nav-button-prev, .nav-button-next, .exif-tag")
+      .forEach(toggle);
+  }
+
+  _wireDownload() {
+    document.getElementById("download").href = this.image_high_url;
+  }
+
+  _wirePinch() {
+    document.addEventListener("touchstart", (e) => {
+      if (e.touches.length === 2) {
+        this._swapToHigh();
       }
+    });
+  }
 
-      global_photo_high.style.transform = global_photo.style.transform;
-      global_photo_high.classList.remove("hide");
+  async _wireNav() {
+    const photo_names = await getPhotoNames();
+    const index = photo_names.findIndex((photo) => {
+      return photo === this.image_high_url;
     });
 
-    global_photo.removeEventListener("transitionend", swapToHigh);
-  }, 50);
-}
-
-function zoom(e) {
-  // Cancel high res if zoom level is about to change
-  global_photo_high.onload = undefined;
-
-  if (global_photo.classList.contains("photo-zoom")) {
-    unWireMouseMove();
-
-    global_photo.classList.add("smooth-zoom");
-    global_photo.classList.replace("photo-zoom", "photo-normal");
-
-    centerPhoto(
-      global_photo.getBoundingClientRect().width / global_zoom_factor
-    );
-
-    global_photo.addEventListener("transitionend", stopSmoothZoom);
-  } else {
-    unWireZoom();
-    global_photo.classList.add("smooth-zoom");
-    initialZoomOnPointer(e);
-    global_photo.classList.replace("photo-normal", "photo-zoom");
-    global_photo.addEventListener("transitionend", wireMouseMove);
-    global_photo.addEventListener("transitionend", stopSmoothZoom);
-    global_photo.addEventListener("transitionend", swapToHigh);
-    global_photo.addEventListener("transitionend", wireZoom);
-  }
-}
-
-function onResize() {
-  if (global_photo.classList.contains("photo-normal")) {
-    centerPhoto(global_photo.width);
-  }
-}
-
-function centerPhoto(width) {
-  global_photo.style.transform = `translate(${
-    (document.body.clientWidth - width) / 2
-  }px, 0px)`;
-}
-
-function show() {
-  function toggle(e) {
-    e.classList.toggle("hide");
-  }
-
-  toggle(document.getElementById("loading"));
-
-  toggle(global_photo);
-  centerPhoto(global_photo.getBoundingClientRect().width);
-
-  document
-    .querySelectorAll(".nav-button-prev, .nav-button-next, .exif-tag")
-    .forEach(toggle);
-}
-
-function wireDownload() {
-  document.getElementById("download").href = image_high_url;
-}
-
-function wirePinch() {
-  document.addEventListener("touchstart", (e) => {
-    if (e.touches.length === 2) {
-      swapToHigh();
+    const prevButton = document.querySelector(".nav-button-prev"); // TODO: move to constructor
+    if (index > 0) {
+      prevButton.href = getViewUrl(photo_names[index - 1]);
+    } else {
+      prevButton.classList.add("hide");
     }
-  });
-}
 
-async function wireNav() {
-  const photo_names = await getPhotoNames();
-  const index = photo_names.findIndex((photo) => {
-    return photo === image_high_url;
-  });
-
-  const prevButton = document.querySelector(".nav-button-prev");
-  if (index > 0) {
-    prevButton.href = getViewUrl(photo_names[index - 1]);
-  } else {
-    prevButton.classList.add("hide");
+    const nextButton = document.querySelector(".nav-button-next");
+    if (index < photo_names.length - 1) {
+      nextButton.href = getViewUrl(photo_names[index + 1]);
+    } else {
+      nextButton.classList.add("hide");
+    }
   }
 
-  const nextButton = document.querySelector(".nav-button-next");
-  if (index < photo_names.length - 1) {
-    nextButton.href = getViewUrl(photo_names[index + 1]);
-  } else {
-    nextButton.classList.add("hide");
+  async _onPhotoLoad() {
+    await loadEXIF(this.global_photo);
+    this._wireDownload();
+    this._wirePinch();
+    this._wireZoom();
+    this._wireSwapToLow();
+    this._wireNav();
+    if (!relyOnPinchToZoom()) {
+      window.addEventListener("resize", this._onResize);
+    }
+    this._show();
+  }
+
+  start() {
+    this.global_photo.src = this.image_low_url;
+    this.global_photo.onload = this._onPhotoLoad;
   }
 }
 
@@ -255,24 +289,11 @@ function getLowUrl(image_high_url) {
   return `${parts[0]}_low.${parts[1]}`;
 }
 
-function view() {
+function init() {
   const url = new URL(window.location.href);
-  image_high_url = url.searchParams.get("url");
-  image_low_url = getLowUrl(image_high_url);
-
-  global_photo.src = image_low_url;
-  global_photo.onload = async () => {
-    await loadEXIF();
-    wireDownload();
-    wirePinch();
-    wireZoom();
-    wireSwapToLow();
-    wireNav();
-    if (!relyOnPinchToZoom()) {
-      window.addEventListener("resize", onResize);
-    }
-    show();
-  };
+  const image_high_url = url.searchParams.get("url");
+  const viewer = new Viewer(getLowUrl(image_high_url), image_high_url);
+  viewer.start();
 }
 
-view();
+init();
